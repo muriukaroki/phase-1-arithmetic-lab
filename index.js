f5159866e61f5a015eb40f13bf869eca55b8250f
Justin <justin.kjaroki@student.moringaschool.com>
2022-08-21 01:36:25 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 19;
+const num4 = 5;
 
-// Write your code here
+const multiply = (num1*num2);
+const mod = num3 % num4;
+
+const random = Math.floor(Math.random() * 10 + 1);
+const max = Math.max(20, 15, 10, 5, 1);
